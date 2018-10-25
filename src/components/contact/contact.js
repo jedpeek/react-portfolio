3aa04c4c56b1c5895106f54de644c3ca35d90173
Jed Peek <jedpeek@Jeds-MacBook-Pro.local>
2018-10-25 13:53:15 -0700

    contact form redirect

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -19,24 +19,24 @@ export default class Contact extends Component {
         <div className="row">
           <div className="eight columns">
             {/* form */}
-            <form action id="contactForm" name="contactForm" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+            <form action='/' id="contactForm" name="contactForm" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contactForm" />
               <fieldset>
                 <div>
                   <label htmlFor="contactName">Name <span className="required">*</span></label>
-                  <input type="text" defaultValue size={35} id="contactName" name="contactName" placeholder="Name" />
+                  <input type="text" defaultValue size={35} id="contactName" name="contactName" defaultValue={"Name"}  required/>
                 </div>
                 <div>
                   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                  <input type="email" defaultValue size={35} id="contactEmail" name="contactEmail" placeholder="Email"  />
+                  <input type="email" defaultValue size={35} id="contactEmail" name="contactEmail" defaultValue={"Email"}  required/>
                 </div>
                 <div>
                   <label htmlFor="contactSubject">Subject</label>
-                  <input type="text" defaultValue size={35} id="contactSubject" name="contactSubject"  placeholder="Subject" />
+                  <input type="text" defaultValue size={35} id="contactSubject" name="contactSubject"  defaultValue={"Subject"}/>
                 </div>
                 <div>
                   <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                  <textarea cols={50} rows={5} id="contactMessage" name="contactMessage" defaultValue={""} />
+                  <textarea cols={50} rows={5} id="contactMessage" name="contactMessage" defaultValue={""} required/>
                 </div>
                 <div>
                   <button type='submit' className="customSubmit">Submit</button>
